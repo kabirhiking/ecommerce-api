a1a4730427c0887baa09240fa1d0e6ae8bb23e4e
kabir <kabirraihan249@gmail.com>
2025-07-21 15:01:47 +0600

    setup project , install dependencies , virtual env and create models

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    
+    orders = relationship("Order", back_populates="user")
+    cart_items = relationship("CartItem", back_populates="user")
+    
+class Product(Base):
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
+    
+class Order(Base):
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    total_price = Column(float)
+    
+    user = relationship("User", back_populates="orders")
+    
+class CartItem(Base):
+    __tablename__ = "cart_items"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    product_id = Column(Integer, ForeignKey("products.id"))
+    quantity = Column(Integer)
+    
+    user = relationship("User", back_populates="cart_items")
+    product = relationship("Product")
+    
