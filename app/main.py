8492fd40cfe3c55121f0ff0d1c27c9d793333c5c
kabir <kabirraihan249@gmail.com>
2025-07-21 17:35:40 +0600

    set to all ruouters

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from app import models, database
+from app.routes import auth, users, products, orders,cart
+
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI(title="E-Commerce API", version="1.0.0")
+
+
+app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(products.router)
+app.include_router(orders.router)
+app.include_router(cart.router)
